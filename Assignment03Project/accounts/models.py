6d300dfcb8ba05b13e29b9d35ec5d69a53c4d1aa
Sam Hatch <samuel.hatch@itas.ca>
2025-04-13 19:08:26 -0700

    files missed in last push

--- Assignment03Project/accounts/models.py
+++ Assignment03Project/accounts/models.py
@@ -3,7 +3,14 @@ from django.contrib.auth.models import User
 
 class Person(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user_group = models.CharField(max_length=10)
+    user_group = models.ForeignKey('Group', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.user.username} ({self.user_group})"
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.ManyToManyField(Person, through='user_group')
+
+    def __str__(self):
+        return self.name
